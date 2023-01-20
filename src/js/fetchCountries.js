346c905f27fa97d1261859c6edc6a22802c6c69b
yanishevska <yanishevska1989@gmail.com>
2023-01-20 19:56:18 +0100

    fix api

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export default function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+  return fetch(`https://restcountries.com/v3.1/name/${searchQuery}`)
         .then(response => response.json())
         .catch(err => console.log(err))
 }
